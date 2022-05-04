b8af59e9392ddfaacdf29d45df45f95366bd3d27
theb0b12 <103223932+theb0b12@users.noreply.github.com>
2022-05-04 13:33:44 -0600

    message

---
+++ ceaserbreaker.py
@@ -0,0 +1,14 @@
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345
+      67890 !?.'
+for key in range(len(SYMBOLS)):
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
+            translatedIndex = symbolIndex - key
+            if translatedIndex < 0:
+                translatedIndex = translatedIndex + len(SYMBOLS)
+             translated = translated + SYMBOLS[translatedIndex]
+        else:
+            translated = translated + symbol
+     print('Key #%s: %s' % (key, translated))
